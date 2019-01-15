cd19aa582f1852b1a0c1417a0694c2753bff0056
minhan <alsgks1218@gmail.com>
2019-01-15 14:06:36 +0900

    add AppLoading and fontIcon

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { AppLoading, Asset, Font } from "expo";
+import { MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+
   render() {
+    const { isLoadingComplete } = this.state;
+
+    if (!isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetAsync}
+          onFinish={this._handleLoadingFinish}
+          onError={this._handleLoadingError}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
         <View style={styles.upper}>
@@ -14,6 +32,28 @@ export default class App extends React.Component {
       </View>
     );
   }
+
+  _loadAssetAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require("./assets/images/readybeIcon192.png"),
+        require("./assets/images/readybeSplash.png")
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font
+      })
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    console.error(error);
+  };
+
+  _handleLoadingFinish = async () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
 }
 
 const styles = StyleSheet.create({
